6503e6e08d68da490fe1c7c582c5f90656033c3f
Thomas Lamprecht <t.lamprecht@proxmox.com>
2021-03-05 16:21:30 +0100

    machine list: save as JSON
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- debian/parse-machines.pl
+++ debian/parse-machines.pl
@@ -3,7 +3,9 @@
 use warnings;
 use strict;
 
-my @machines = ();
+use JSON;
+
+my $machines = [];
 
 while (<STDIN>) {
     if (/^\s*Supported machines are:/) {
@@ -12,10 +14,14 @@ while (<STDIN>) {
 
     s/^\s+//;
     my @machine = split(/\s+/);
-    next if $machine[0] !~ m/^pc-(i440fx|q35)/;
-    push @machines, $machine[0];
+    next if $machine[0] !~ m/^pc-(i440fx|q35)-(.+)$/;
+    push @$machines, {
+        'id' => $machine[0],
+        'type' => $1,
+        'version' => $2,
+    };
 }
 
-die "no QEMU machine types detected from STDIN input" if scalar (@machines) <= 0;
+die "no QEMU machine types detected from STDIN input" if scalar (@$machines) <= 0;
 
-print join("\n", @machines) or die "$!\n";
+print to_json($machines, { utf8 => 1 }) or die "$!\n";
